ceccec9175a410c0e08d181e48b7fd8600fc2276
bravo <bravod@apc.edu.ph>
2019-12-20 10:33:10 +0800

    no message

--- Modules/Test/Resources/views/components/actions.blade.php
+++
@@ -1,9 +0,0 @@
-@can('edit-test')
-    <a href="{{ route('test.edit', $test->id) }}" class="btn btn-xs btn-outline-primary">
-        <i class="fa fa-pencil-alt"></i>
-    </a>
-@endcan()
-
-@can('delete-test')
-    <button class="btn btn-xs btn-outline-danger"><i class="fa fa-trash"></i></button>
-@endcan
