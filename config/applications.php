55d21ad5aeb513623b85c3ec91220fcad593302a
bravo <bravod@apc.edu.ph>
2020-01-10 11:35:35 +0800

    Create api proxy handle for axios to secure access token Start tracking passsport keys to make it it the same accross all star application Create new personal access token every user logged in and revoke all previous token store personal access token to laravel session

---
+++ config/applications.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    'admin' => [
+        'name' => 'Sun',
+        'url' => 'core.test'
+    ],
+    'admission' => [
+        'name' => 'Alphard',
+        'url' => 'alphard.star.apc.edu.ph'
+    ],
+    'human_resource' => [
+        'name' => 'Betelgeuse',
+        'url' => 'http://betelgeuse.star.apc.edu.ph'
+    ],
+    'accounting' => [
+        'name' => 'Bellatrix',
+        'url' => 'http://bellatrix.star.apc.edu.ph'
+    ],
+    'library' => [
+        'name' => 'Pollux',
+        'url' => 'http://pollux.star.apc.edu.ph'
+    ],
+    'payroll' => [
+        'name' => 'Castor',
+        'url' => 'http://castor.star.apc.edu.ph'
+    ],
+    'student_records' => [
+        'name' => 'Vega',
+        'url' => 'http://vega.star.apc.edu.ph'
+    ],
+    'student_services' => [
+        'name' => 'Altair',
+        'url' => 'http://altair.star.apc.edu.ph'
+    ],
+    'external_relations' => [
+        'name' => 'Rigel',
+        'url' => 'http://rigel.star.apc.edu.ph'
+    ],
+    'teacher_services' => [
+        'name' => 'Sirius',
+        'url' => 'http://sirius.star.apc.edu.ph'
+    ],
+    'logistics' => [
+        'name' => 'Capella',
+        'url' => 'http://capella.star.apc.edu.ph'
+    ],
+    'building_admin' => [
+        'name' => 'Mizar',
+        'url' => 'http://mizar.star.apc.edu.ph'
+    ],
+    'service_desk' => [
+        'name' => 'Algol',
+        'url' => 'http://algol.star.apc.edu.apc.edu.ph'
+    ],
+    'events' => [
+        'name' => 'Antares',
+        'url' => 'http://antares.star.apc.edu.ph'
+    ],
+    'general' => [
+        'name' => 'Canopus',
+        'url' => 'http://canopus.star.apc.edu.ph'
+    ]
+];
