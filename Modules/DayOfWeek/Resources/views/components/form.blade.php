08505bb040107a03a5f5b03e4f6e8b1a0c3fce40
REYMONDT\reymond <reymondt@apc.edu.ph>
2020-01-15 11:17:51 +0800

    Customize the Input field into select field.

--- Modules/DayOfWeek/Resources/views/components/form.blade.php
+++ Modules/DayOfWeek/Resources/views/components/form.blade.php
@@ -1,8 +1,20 @@
 <el-form :model="form" :rules="rules" ref="createForm" @submit.native.prevent="submitForm('createForm')">
     <el-row :gutter="10">
         {{--Day String--}}
+{{--        <el-form-item label="Day" prop="day_string">--}}
+{{--            <el-input v-model="form.day_string"></el-input>--}}
+{{--        </el-form-item>--}}
+
         <el-form-item label="Day" prop="day_string">
-            <el-input v-model="form.day_string"></el-input>
+            <el-select v-model="form.day_string" placeholder="please select day here">
+                <el-option label="Monday" value="Monday"></el-option>
+                <el-option label="Tuesday" value="Tuesday"></el-option>
+                <el-option label="Wednesday" value="Wednesday"></el-option>
+                <el-option label="Thursday" value="Thursday"></el-option>
+                <el-option label="Friday" value="Friday"></el-option>
+                <el-option label="Saturday" value="Saturday"></el-option>
+                <el-option label="Sunday" value="Sunday"></el-option>
+            </el-select>
         </el-form-item>
 
         <el-form-item class="text-right">
