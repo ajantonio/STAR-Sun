e57314e24ec85978ff36eca925eced6ba4ee4a51
bravo <bravod@apc.edu.ph>
2020-04-28 16:48:34 +0800

    fix null values

--- Modules/Link/Actions/StoreNewLink.php
+++ Modules/Link/Actions/StoreNewLink.php
@@ -47,7 +47,7 @@ class StoreNewLink extends Action
         $link->id = Str::orderedUuid()->toString();
         $link->application_id = $this->application['id'];
         $link->resource_group_id = $this->resource_group['id'];
-        $link->parent_link_id = $this->parent_link['id'];
+        $link->parent_link_id = $this->parent_link['id'] ?? null;
         $link->title = $this->title;
         $link->description = $this->description;
         $link->url = $this->url;
@@ -55,7 +55,7 @@ class StoreNewLink extends Action
         $link->active_pattern = $this->active_pattern;
         $link->order = $this->order;
         $link->status = 'On';
-        $link->permission_id = $this->permission['id'];
+        $link->permission_id = $this->permission['id'] ?? null;
         $link->save();
         return $link;
     }
