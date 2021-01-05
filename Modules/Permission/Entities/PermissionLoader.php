8888dacdec383bbe1a71665ba7dd2a6891600ae8
Bravo <bravod@apc.edu.ph>
2021-01-05 11:42:22 +0800

    PermissionLoader.php edited online with Bitbucket
    temporarily remove application filtering on permisssion loader

--- Modules/Permission/Entities/PermissionLoader.php
+++ Modules/Permission/Entities/PermissionLoader.php
@@ -21,8 +21,8 @@ class PermissionLoader extends BasePermission
             $builder->where('active', '=', 'Yes');
         });
 
-        static::addGlobalScope('application', function (Builder $builder) {
-            $builder->where('application_id', '=', config('app.id'));
-        });
+        //static::addGlobalScope('application', function (Builder $builder) {
+         //   $builder->where('application_id', '=', config('app.id'));
+        //});
     }
 }
